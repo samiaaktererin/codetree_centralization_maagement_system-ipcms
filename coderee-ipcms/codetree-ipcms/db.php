911b676a32d51a57f937abd3cf9f110b43429a0f
Samia Akter Erin <142722098+samiaaktererin@users.noreply.github.com>
2026-08-17 18:36:44 +0600

    Delete coderee-ipcms directory

--- coderee-ipcms/codetree-ipcms/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=codetree", "root", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
-}
-?>
-
